5cff6d1a0874bbc41a60bff47b8ba6f3f4f972fb
vytlaranadheer02 <vytlaranadheer02@gmail.com>
2022-12-10 17:31:17 +0530

    first commit

---
+++ Amazon/Controllers/RegistrationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Amazon.Models;
+
+namespace Amazon.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        StoreDbContext dc = new StoreDbContext();
+
+        public ViewResult UserRegister()
+        {
+            Registration re = new Registration();
+            return View(re);
+        }
+        [HttpPost]
+        public string AddUser(Registration registration)
+        {
+            dc.Registrations.Add(registration);
+            dc.SaveChanges();
+            return "User added successfully";
+        }
+    }
+}
---
+++ Amazon/Models/StoreDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Amazon.Models
+{
+    public class StoreDbContext : DbContext
+    {
+        public DbSet<Registration> Registrations { get; set; }
+    }
+}
